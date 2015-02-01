fd9c50b49dfceed34d4ec1db7e7d2c591915a880
phreaker2600 <swwiley@gmail.com>
2015-02-01 19:40:16 +0000

    works sort of but is messy

--- batmon.c
+++ batmon.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h> 
+#include <sys/types.h>
+#include <dirent.h>
 void
 die(char *message)
 {
@@ -7,9 +10,29 @@ die(char *message)
 	exit(1);
 }
 char *
-getbatpath(int stop)
+getbatname(int stop, char *buf)
 {
-	return "/sys/class/power_supply/battery/capacity";
+	DIR *bats = opendir("/sys/class/power_supply/");
+	struct dirent *cbat;
+	stop=stop+2;
+	for(;
+		stop>0 &&
+		(cbat=readdir(bats));
+		stop--
+	);
+	strcpy(buf,cbat->d_name);
+	return buf;
+}
+char *
+getbatpath(char *name , char *buf)
+{
+	puts(buf);
+	//strcpy(buf,cbat->d_name);
+	strcat(buf,"/sys/class/power_supply/");
+	puts(buf);
+	strcat(buf,name);
+	strcat(buf,"/capacity");
+	return buf;
 }
 int
 getbat(char *batpath)
@@ -23,7 +46,9 @@ getbat(char *batpath)
 		return bc;
 	}
 	else
-		die("Can't open sysfs battery device, check permisions\n");
+	{
+		die(batpath);
+	}
 }
 /*void
 percentbar(char *message,int n,int w)
@@ -43,7 +68,12 @@ percentbar(char *message,int n,int w)
 int
 main()
 {
+	char buff[256];
+	char *buf=&buff[0];
+	*buf=0;
+	char buff2[512];
+	buff2[0]=0;
 //	percentbar("battery: ",getbat(),80);
-	printf("%d",getbat(getbatpath(1)));
+	printf("%d",getbat(getbatpath(getbatname(2,buf),buff2)));
 	return 0;
 }
